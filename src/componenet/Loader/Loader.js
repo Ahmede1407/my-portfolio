2fe9ba9eb9134a262cc7dd69cd974806f3935895
Ahmed <ahmed.mano.am68@gmail.com>
2021-09-26 18:56:05 +0200

    removed useless code.

--- src/componenet/Loader/Loader.js
+++ src/componenet/Loader/Loader.js
@@ -1,27 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./loader.css";
 
 export default function App() {
-  //   const [isLoading, setLoading] = useState(true);
-
-  //   function fakeRequest() {
-  //     return new Promise((resolve) => setTimeout(() => resolve(), 2500));
-  //   }
-
-  //   useEffect(() => {
-  //     fakeRequest().then(() => {
-  //       const el = document.querySelector(".loader-container");
-  //       if (el) {
-  //         el.remove();
-  //         setLoading(!isLoading);
-  //       }
-  //     });
-  //   }, []);
-
-  //   if (isLoading) {
-  //     return null;
-  //   }
-
   return (
     <div id="loading">
       <div className="loader">
